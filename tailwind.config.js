37fec3c7fcb9cd4863a140dbbaddee9c900e6c03
unknown <mariano.alvarez.personal@gmail.com>
2024-06-10 15:46:56 -0300

    Initial tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: ["./index.html"],
   theme: {
-    extend: {},
+    colors: {
+      mainColor: '#0697DC',
+      mainBlue: '#008CD6'
+    },
+    extend: {
+      fontFamily: {
+        Araside: ['Araside', 'sans-serif'],
+      },
+    },
     screens: {
       'sm': '640px',
       'md': '768px',  
