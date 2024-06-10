33fec3e258ea3ea2a48ba4122ce88eaf99563441
unknown <mariano.alvarez.personal@gmail.com>
2024-06-10 14:22:08 -0300

    Initial config commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {},
+    screens: {
+      'sm': '640px',
+      'md': '768px',  
+      'lg': '1024px',
+    },
+  },
+  variants: {},
+  plugins: [],
+}
